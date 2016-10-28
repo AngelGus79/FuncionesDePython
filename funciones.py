84bbf7c8a6da0a478676f7d19ae4fd26e837c3b4
AngelGus79 <agustavo79@gmail.com>
2016-10-28 00:44:52 -0500

    correccion de errores flake

--- funciones.py
+++ funciones.py
@@ -1,11 +1,9 @@
 from collections import Counter
 
-    
+
 def cuenta_elementos_de_lista(lista):
     """
     >>> cuenta_elementos_de_lista([1, 1, 2, 2, 3, 4, 5, 6])
     Counter({1: 2, 2: 2, 3: 1, 4: 1, 5: 1, 6: 1})
     """
     return Counter(lista)
-
-
