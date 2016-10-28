ddad6d17c91f007d8a2f1fef9f57533be9d963dd
AngelGus79 <agustavo79@gmail.com>
2016-10-28 00:06:31 -0500

    creacion de primera funcion

--- funciones.py
+++ funciones.py
@@ -1,6 +1,8 @@
 from collections import Counter
-l = [1, 1, 2, 2, 3, 4, 5, 6]
-c = Counter(l)
-
-for x, y in c.items():
-    print(x, y)
+    
+def cuenta_elementos_de_lista(lista):
+    """
+    >>> zero_sum_list([1, 1, 2, 2, 3, 4, 5, 6])
+    Counter({1: 2, 2: 2, 3: 1, 4: 1, 5: 1, 6: 1})
+    """
+    
