bfbac6f6be27a0bcd0a2451abfbc8647dced926a
AngelGus79 <agustavo79@gmail.com>
2016-10-28 00:36:32 -0500

    Correccion de prueba unitaria

--- funciones.py
+++ funciones.py
@@ -3,7 +3,7 @@ from collections import Counter
     
 def cuenta_elementos_de_lista(lista):
     """
-    >>> zero_sum_list([1, 1, 2, 2, 3, 4, 5, 6])
+    >>> cuenta_elementos_de_lista([1, 1, 2, 2, 3, 4, 5, 6])
     Counter({1: 2, 2: 2, 3: 1, 4: 1, 5: 1, 6: 1})
     """
     return Counter(lista)
